4689034a08773b8243ad1841e34a27d6cd5b5dd1
Nikolay <kolambo4ka@gmail.com>
2024-12-15 21:27:48 +0200

    returned deafult applyTransction

--- core/state_processor.go
+++ core/state_processor.go
@@ -142,6 +142,8 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 
 	return statedb, receipts, allLogs, *usedGas, nil
 }
+
+// /////////////////ORIGINAL FUNCTION////////////////////////////
 func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, statedb *state.StateDB, blockNumber *big.Int, blockHash common.Hash, tx *types.Transaction, usedGas *uint64, evm *vm.EVM, receiptProcessors ...ReceiptProcessor) (*types.Receipt, error) {
 	// Create a new context to be used in the EVM environment.
 	txContext := NewEVMTxContext(msg)
@@ -152,13 +154,6 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 	if err != nil {
 		return nil, err
 	}
-	log.Warn("PIZDA compu  apply message has gone", result)
-	customGasFee, gasErr := getCustomGasFeeFromContract(msg, evm)
-	log.Warn("call to contract", customGasFee)
-	if gasErr == nil && customGasFee > 0 {
-		// Override the gas used with the custom value from the contract
-		result.UsedGas = customGasFee
-	}
 
 	// Update the state with pending changes.
 	var root []byte
@@ -168,7 +163,6 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 		root = statedb.IntermediateRoot(config.IsEIP158(blockNumber)).Bytes()
 	}
 	*usedGas += result.UsedGas
-	log.Warn("PIZDA compu usedGas amount in this quantity", usedGas)
 
 	// Create a new receipt for the transaction, storing the intermediate root and gas used
 	// by the tx.
@@ -196,6 +190,61 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 	}
 	return receipt, err
 }
+
+// func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, statedb *state.StateDB, blockNumber *big.Int, blockHash common.Hash, tx *types.Transaction, usedGas *uint64, evm *vm.EVM, receiptProcessors ...ReceiptProcessor) (*types.Receipt, error) {
+// 	// Create a new context to be used in the EVM environment.
+// 	txContext := NewEVMTxContext(msg)
+// 	evm.Reset(txContext, statedb)
+
+// 	// Apply the transaction to the current state (included in the env).
+// 	result, err := ApplyMessage(evm, msg, gp)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	log.Warn("PIZDA compu  apply message has gone", result)
+// 	customGasFee, gasErr := getCustomGasFeeFromContract(msg, evm)
+// 	log.Warn("call to contract", customGasFee)
+// 	if gasErr == nil && customGasFee > 0 {
+// 		// Override the gas used with the custom value from the contract
+// 		result.UsedGas = customGasFee
+// 	}
+
+// 	// Update the state with pending changes.
+// 	var root []byte
+// 	if config.IsByzantium(blockNumber) {
+// 		statedb.Finalise(true)
+// 	} else {
+// 		root = statedb.IntermediateRoot(config.IsEIP158(blockNumber)).Bytes()
+// 	}
+// 	*usedGas += result.UsedGas
+// 	log.Warn("PIZDA compu usedGas amount in this quantity", usedGas)
+
+// 	// Create a new receipt for the transaction, storing the intermediate root and gas used
+// 	// by the tx.
+// 	receipt := &types.Receipt{Type: tx.Type(), PostState: root, CumulativeGasUsed: *usedGas}
+// 	if result.Failed() {
+// 		receipt.Status = types.ReceiptStatusFailed
+// 	} else {
+// 		receipt.Status = types.ReceiptStatusSuccessful
+// 	}
+// 	receipt.TxHash = tx.Hash()
+// 	receipt.GasUsed = result.UsedGas
+
+// 	// If the transaction created a contract, store the creation address in the receipt.
+// 	if msg.To == nil {
+// 		receipt.ContractAddress = crypto.CreateAddress(evm.TxContext.Origin, tx.Nonce())
+// 	}
+
+// 	// Set the receipt logs and create the bloom filter.
+// 	receipt.Logs = statedb.GetLogs(tx.Hash(), blockNumber.Uint64(), blockHash)
+// 	receipt.BlockHash = blockHash
+// 	receipt.BlockNumber = blockNumber
+// 	receipt.TransactionIndex = uint(statedb.TxIndex())
+// 	for _, receiptProcessor := range receiptProcessors {
+// 		receiptProcessor.Apply(receipt)
+// 	}
+// 	return receipt, err
+// }
 
 // func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, statedb *state.StateDB, blockNumber *big.Int, blockHash common.Hash, tx *types.Transaction, usedGas *uint64, evm *vm.EVM, receiptProcessors ...ReceiptProcessor) (*types.Receipt, error) {
 // 	// Create a new context to be used in the EVM environment.
